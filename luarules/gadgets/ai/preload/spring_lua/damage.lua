34e2ddb5102439da0aa661c9235748abb7819e83
eronoobos <eronoobos@gmail.com>
2016-07-18 15:37:21 -0700

    fix attacker in UnitDamaged, fill in damage object functions

--- luarules/gadgets/ai/preload/spring_lua/damage.lua
+++ luarules/gadgets/ai/preload/spring_lua/damage.lua
@@ -2,14 +2,37 @@ ShardSpringDamage = class(function(a)
    --
 end)
 
+local spGetGameFrame = Spring.GetGameFrame
+local spGetProjectileDirection = Spring.GetProjectileDirection
 
-function ShardSpringDamage:Init( damage, weaponDefId, paralyzer )
+function ShardSpringDamage:Init( damage, weaponDefID, paralyzer, projectileID, engineAttacker )
 	self.damage = damage
-	self.weaponDefId = weaponDefId
+	self.weaponDefID = weaponDefID
 	self.paralyzer = paralyzer
-	self.gameframe = Spring.GetGameFrame()
+	self.projectileID = projectileID
+	self.attacker = engineAttacker
+	self.gameframe = spGetGameFrame()
+	self.direction = spGetProjectileDirection(projectileID)
+	self.damageType = weaponDefID
+	self.weaponType = WeaponDefs[weaponDefID].name
 end
 
 function ShardSpringDamage:Damage()
 	return self.damage
+end
+
+function ShardSpringDamage:Attacker()
+	return self.attacker
+end
+
+function ShardSpringDamage:Direction()
+	return self.direction
+end
+
+function ShardSpringDamage:DamageType()
+	return self.damageType
+end
+
+function ShardSpringDamage:WeaponType()
+	return self.weaponType
 end
